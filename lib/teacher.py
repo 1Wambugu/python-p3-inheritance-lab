81f43c3daf1495d214a91252d2c160d0bce31f31
1Wambugu <martingakuya@gmail.com>
2023-10-20 15:31:17 +0300

    Completed lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
-
-from user import User
-
+# lib/teacher.py
+from user import User  # Import the User class
 import random
 
 class Teacher(User):
+    knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+    ]
 
     def teach(self):
-        pass
+        import random
+        index = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[index]
